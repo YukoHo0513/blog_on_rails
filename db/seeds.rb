e7f53d7e1782188b82635c16e701f740f807eefd
Yuko <yuko.kumason@gmail.com>
2021-10-16 13:17:18 -0700

    Posts Model prepared, and Posts controller set up (index page).

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+
+50.times do
+    created_at = Faker::Date.backward(days:365 * 5)
+
+    Post.create(
+        title: Faker::Kpop.ii_groups,
+        body: Faker::GreekPhilosophers.quote * 50,
+        created_at: created_at,
+        updated_at: created_at
+    )
+end
+
+posts = Post.all
+
+puts Cowsay.say("Generated #{posts.count} posts", :frogs)
+
+
+
